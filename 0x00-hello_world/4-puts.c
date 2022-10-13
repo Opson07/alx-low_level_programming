91d0fa26f500953c3df957311d4bf320c7403ac2
Opson07 <opeyemishoyemi44@gmail.com>
2022-10-13 07:53:25 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 
+	return (0);
+}
